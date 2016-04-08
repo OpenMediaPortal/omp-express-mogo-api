af55ffa9d3e2ffc6ad82d7ca264adc449c74c8ca
Oliver Jourmel <oliver.jourmel@gmail.com>
2016-04-08 00:17:40 -0700

    Update server to support CORS
            * Since the frontend and backend run on different ports,
              Cross Origin Requests must be enabled.

--- server.js
+++ server.js
@@ -36,6 +36,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Multer can be used to upload files from multipart/form-data
 // app.use(multer({dest:'./uploads/'}).single('photo'));
 
+// CORS
+// @see http://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Serve endpoint code
 app.get('/sync', sync.index);
 app.get('/sync/:libkey', sync.show);
